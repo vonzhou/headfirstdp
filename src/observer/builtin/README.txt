d1834427461f506329f4fee3a0c745aeefdc0955
vonzhou <vonzhou@163.com>
2014-11-24 10:57:31 +0800

    Java内置观察者模式

---
+++ src/observer/builtin/README.txt
@@ -0,0 +1,2 @@
+
+2 Java内置的观察者模式（Observable，Observer）
---
+++ src/observer/builtin/WeatherStation.java
@@ -0,0 +1,19 @@
+package observer.builtin;
+
+public class WeatherStation {
+
+	public static void main(String[] args) {
+		WeatherData weatherData = new WeatherData();
+
+		new CurrentConditionsDisplay(weatherData);
+		new StatisticsDisplay(weatherData);
+		new ForecastDisplay(weatherData);
+		new HeatIndexDisplay(weatherData);
+
+		weatherData.setMeasurements(80, 65, 30.4f);
+		System.out.println("======================");
+		weatherData.setMeasurements(82, 70, 29.2f);
+		System.out.println("======================");
+		weatherData.setMeasurements(78, 90, 29.2f);
+	}
+}
