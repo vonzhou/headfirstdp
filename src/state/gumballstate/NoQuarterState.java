80ea162a23b324c830de969963189b21418fe5e7
vonzhou <vonzhou@163.com>
2014-11-27 10:12:12 +0800

    每个状态独立封装

---
+++ src/state/gumballstate/NoQuarterState.java
@@ -0,0 +1,31 @@
+package state.gumballstate;
+
+public class NoQuarterState implements State {
+	// 通过这个指向贩卖机的指针 更新状态
+	GumballMachine gumballMachine;
+
+	public NoQuarterState(GumballMachine gumballMachine) {
+		this.gumballMachine = gumballMachine;
+	}
+
+	public void insertQuarter() {
+		System.out.println("You inserted a quarter");
+		gumballMachine.setState(gumballMachine.getHasQuarterState());
+	}
+
+	public void ejectQuarter() {
+		System.out.println("You haven't inserted a quarter");
+	}
+
+	public void turnCrank() {
+		System.out.println("You turned, but there's no quarter");
+	}
+
+	public void dispense() {
+		System.out.println("You need to pay first");
+	}
+
+	public String toString() {
+		return "waiting for quarter";
+	}
+}
