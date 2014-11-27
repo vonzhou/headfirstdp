07e9a8c3bd36934f4a274163c1ecabe4980c43cd
vonzhou <vonzhou@163.com>
2014-11-27 10:18:40 +0800

    新加一个状态

---
+++ src/state/gumballwiner/HasQuarterState.java
@@ -0,0 +1,40 @@
+package state.gumballwiner;
+
+import java.util.Random;
+
+public class HasQuarterState implements State {
+	Random randomWinner = new Random(System.currentTimeMillis());
+	GumballMachine gumballMachine;
+ 
+	public HasQuarterState(GumballMachine gumballMachine) {
+		this.gumballMachine = gumballMachine;
+	}
+  
+	public void insertQuarter() {
+		System.out.println("You can't insert another quarter");
+	}
+ 
+	public void ejectQuarter() {
+		System.out.println("Quarter returned");
+		gumballMachine.setState(gumballMachine.getNoQuarterState());
+	}
+ 
+	public void turnCrank() {
+		System.out.println("You turned...");
+		// 随机产生一个赢家，决定了以一个状态的变迁
+		int winner = randomWinner.nextInt(10);
+		if ((winner == 0) && (gumballMachine.getCount() > 1)) {
+			gumballMachine.setState(gumballMachine.getWinnerState());
+		} else {
+			gumballMachine.setState(gumballMachine.getSoldState());
+		}
+	}
+
+    public void dispense() {
+        System.out.println("No gumball dispensed");
+    }
+ 
+	public String toString() {
+		return "waiting for turn of crank";
+	}
+}
