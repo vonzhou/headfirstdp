376bffab2ed3ae5c031d3aa5275ebb2b091bf24d
vonzhou <vonzhou@163.com>
2014-11-27 10:31:44 +0800

    增加监视器

---
+++ src/proxy/gumballmonitor/NoQuarterState.java
@@ -0,0 +1,34 @@
+package proxy.gumballmonitor;
+
+public class NoQuarterState implements State {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 86186069015107861L;
+	GumballMachine gumballMachine;
+
+	public NoQuarterState(GumballMachine gumballMachine) {
+		this.gumballMachine = gumballMachine;
+	}
+
+	public void insertQuarter() {
+		System.out.println("You inserted a quarter");
+		gumballMachine.setState(gumballMachine.getHasQuarterState());
+	}
+
+	public void ejectQuarter() {
+		System.out.println("You haven't inserted a quarter");
+	}
+
+	public void turnCrank() {
+		System.out.println("You turned, but there's no quarter");
+	}
+
+	public void dispense() {
+		System.out.println("You need to pay first");
+	}
+
+	public String toString() {
+		return "waiting for quarter";
+	}
+}
